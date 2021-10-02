478eefdcdf010554fbf373ee47d0470bc32c1615
hyosang-digipen <70678504+hyosang-digipen@users.noreply.github.com>
2021-10-03 02:16:46 +0900

    Update main.cpp

--- BIT_SAVER/main.cpp
+++ BIT_SAVER/main.cpp
@@ -1,17 +1,3 @@
-/*!
-@file    main.cpp
-@author  pghali@digipen.edu
-@date    10/11/2016
-
-This file uses functionality defined in types GLHelper and GLApp to initialize
-an OpenGL context and implement a game loop.
-
-*//*__________________________________________________________________________*/
-
-/*                                                                   includes
------------------------------------------------------------------------------ */
-// Extension loader library's header must be included before GLFW's header!!!
-
 #include "Engine/glhelper.h"
 #include "Engine/glapp.h"
 #include <iostream>
@@ -19,9 +5,6 @@ an OpenGL context and implement a game loop.
 #include <iomanip>
 #include"Engine/Engine.h"
 #include"Game/Level1.h"
-static void draw();
-static void update();
-static void cleanup();
 
 
 Engine& engine = Engine::Instance();
@@ -37,65 +20,3 @@ int main()
     }
    // cleanup();
 }
-
-static void update() 
-{
-    glfwPollEvents();
-    double delta_time = GLHelper::update_time(1.0);
-   GLApp::update(delta_time);
-}
-
-static void draw() 
-{
-    GLApp::draw();
-    glfwSwapBuffers(engine.GetWindow().ptr_window);
-}
-
-void cleanup() {
-
-    GLHelper::cleanup();
-
-    GLApp::cleanup();
-}
-
-//
-//#include <stdexcept>	//std::exception
-//#include <iostream>		//std::cerr
-//
-//#include "Engine/Engine.h"
-//#include "Game/Splash.h"
-//#include "Game/MainMenu.h"
-//#include "Game/Level1.h"
-//#include "Game/Level2.h"
-//#include "Game/MainMenu.h"
-//#include "Game/Screens.h"
-//
-//
-//int main(void) {
-//    try {
-//        Engine& engine = Engine::Instance();
-//        engine.Init("Week 12");
-//        Splash splash;
-//        MainMenu mainmenu;
-//        Level1 level1;
-//        Level2 level2;
-//
-//        engine.AddSpriteFont("assets/font.png");
-//        engine.AddSpriteFont("assets/font2.png");
-//        engine.GetGameStateManager().AddGameState(splash);
-//        engine.GetGameStateManager().AddGameState(mainmenu);
-//        engine.GetGameStateManager().AddGameState(level1);
-//        engine.GetGameStateManager().AddGameState(level2);
-//
-//        while (engine.HasGameEnded() == false)
-//        {
-//            engine.Update();
-//        }
-//        engine.Shutdown();
-//        return 0;
-//    }
-//    catch (std::exception& e) {
-//        std::cerr << e.what() << "\n";
-//        return -1;
-//    }
-//}
