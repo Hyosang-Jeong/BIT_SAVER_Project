961ce3c936c600990b260d94c46ed6de2b58090e
hyosang-digipen <70678504+hyosang-digipen@users.noreply.github.com>
2021-10-02 21:54:14 +0900

    add gamestate

---
+++ BIT_SAVER/Engine/GameStateManager.cpp
@@ -0,0 +1,105 @@
+/*--------------------------------------------------------------
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name: GameStateManager.cpp
+Project: CS230
+Author: Hyosang Jung
+Creation date: 2021-03-07
+-----------------------------------------------------------------*/
+
+#include"GameStateManager.h"
+#include"GameState.h"
+#include"Engine.h" //Getlogger
+GameStateManager::GameStateManager()
+{
+	currGameState = nullptr;
+	nextGameState = nullptr;
+	state = State::START;
+}
+
+void GameStateManager::AddGameState(GameState& gameState)
+{
+	GameStateManager::gameStates.push_back(&gameState);
+}
+
+void GameStateManager::SetNextState(int initState)
+{
+	nextGameState = gameStates[initState];
+}
+
+void GameStateManager::Shutdown()
+{
+	nextGameState = nullptr;
+}
+void GameStateManager::ReloadState()
+{
+	state = State::UNLOAD;
+}
+void GameStateManager::Update(double dt)
+{
+	switch (state)
+	{
+	case State::START: {
+		if (gameStates.empty() == true)
+		{
+			Engine::GetLogger().LogError("Don't have game states");
+			state = State::SHUTDOWN;
+		}
+		else
+		{
+			nextGameState = gameStates[0];
+			state = State::LOAD;
+		}
+		break;
+	}
+	case State::LOAD:
+	{
+		currGameState = nextGameState;
+		Engine::GetLogger().LogEvent("Load " + currGameState->GetName());
+		currGameState->Load();
+		Engine::GetLogger().LogEvent("Load Complete");
+		state = State::UPDATE;
+		break;
+	}
+	case State::UPDATE:
+	{
+		if (currGameState != nextGameState)
+		{
+			state = State::UNLOAD;
+		}
+		else
+		{
+			Engine::GetLogger().LogVerbose("Update" + currGameState->GetName());
+			currGameState->Update(dt);
+			currGameState->Draw();
+		}
+
+		break;
+	}
+	case State::UNLOAD:
+	{
+		Engine::GetLogger().LogEvent("Unload " + currGameState->GetName());
+		//if (currGameState != nextGameState)
+		//{
+		//	Engine::GetTextureManager().Unload();
+		//}
+		currGameState->Unload();
+
+		if (nextGameState == nullptr)
+		{
+			state = State::SHUTDOWN;
+		}
+		else
+		{
+			state = State::LOAD;
+		}
+		break;
+	}
+	case State::SHUTDOWN:
+	{
+		state = State::EXIT;
+		break;
+	}
+	}
+}
