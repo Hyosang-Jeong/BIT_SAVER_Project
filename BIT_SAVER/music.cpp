dd83023a14e956ce2048404e688412a419433cf7
choijaewoo <wod6983@gmail.com>
2021-10-03 21:09:07 +0900

    add_music filters

--- BIT_SAVER/music.cpp
+++ BIT_SAVER/music.cpp
@@ -10,7 +10,7 @@ void Music::SoundSystem()
 
     pSystem->init(4, FMOD_INIT_NORMAL, NULL);
 
-    pSystem->createSound("music1.mid", FMOD_LOOP_NORMAL | FMOD_DEFAULT, NULL, &pSound[0]);
+    pSystem->createSound("../sound/music1.mid", FMOD_LOOP_NORMAL | FMOD_DEFAULT, NULL, &pSound[0]);
 }
 
 void Music::Play(int Sound_num)
