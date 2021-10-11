349908eb9de1e0fd7ff567daef2335d3fb622966
choijaewoo <wod6983@gmail.com>
2021-10-11 21:32:08 +0900

    change midi,music

--- BIT_SAVER/Engine/Music/music.h
+++ BIT_SAVER/Engine/Music/music.h
@@ -1,31 +1,22 @@
 #pragma once
-
+//we use fmod library here
 #include<iostream>
 #include"fmod.hpp"
 #include"fmod_errors.h"
-
-
+#include <string>
 
 class Music
 {
 public:
-    void SoundSystem();
+    enum SOUND_NAME
+    {
+        MUSIC_CANON = 0,
+        MUSIC_END
+    };
+    void SoundSystem(const char* music_file_name, int music_num);
     void Play(int Sound_num);
 private:
     FMOD::System* pSystem;
-    FMOD::Sound* pSound[1];
-    FMOD::Channel* pChannel[1];
+    FMOD::Sound* pSound[MUSIC_END];
+    FMOD::Channel* pChannel;
 };
-//void SoundSystem() {
-//    System_Create(&pSystem);
-//
-//    pSystem->init(4, FMOD_INIT_NORMAL, NULL);
-//
-//    pSystem->createSound("music1.mid", FMOD_LOOP_NORMAL | FMOD_DEFAULT, NULL, &pSound[0]);
-//    //pSystem->createSound("Festival_of_Ghost.wav", FMOD_DEFAULT, NULL, &pSound[1]);
-//}
-//
-//void Play(int Sound_num) {
-//
-//    pSystem->playSound(pSound[Sound_num], NULL, 0, pChannel);
-//}
