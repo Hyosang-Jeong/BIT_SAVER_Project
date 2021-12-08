d371b76b1223ae1617333c36d267e8b9e31a11a5
sunwoolee-digipen <sunwoo.lee@digipen.edu>
2021-12-09 05:33:04 +0900

    Update Mainmenu.cpp

--- BIT_SAVER/Game/Levels/Mainmenu.cpp
+++ BIT_SAVER/Game/Levels/Mainmenu.cpp
@@ -66,10 +66,10 @@ void Mainmenu::Draw()
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	//mainmenu->Draw(glm::mat3{ 1,0,0,0,1,0,0,0,1 }, model, "Hero");
-	Engine::GetText(font2).Draw("Press 0 for Level0", 0.f, 50.f, 3.f, { 0.5f,0.5f,0.5f });
-	Engine::GetText(font2).Draw("Press 1 for Level1", 0.f, 150.f, 3.f, { 0.5f,0.5f,0.5f });
-	Engine::GetText(font2).Draw("Press 2 for Level2", 0.f, 250.f, 3.f, { 0.5f,0.5f,0.5f });
-	Engine::GetText(font2).Draw("Press 3 for Level3", 0.f, 350.f, 3.f, { 0.5f,0.5f,0.5f });
+	Engine::GetText(font2).Draw("Press 0 for Level 0", 0.f, 50.f, 3.f, { 0.5f,0.5f,0.5f });
+	Engine::GetText(font2).Draw("Press 1 for Level 1", 0.f, 150.f, 3.f, { 0.5f,0.5f,0.5f });
+	Engine::GetText(font2).Draw("Press 2 for Level 2", 0.f, 250.f, 3.f, { 0.5f,0.5f,0.5f });
+	Engine::GetText(font2).Draw("Press 3 for Level 3", 0.f, 350.f, 3.f, { 0.5f,0.5f,0.5f });
 	Engine::GetText(font2).Draw("Press Q to offset", 0.f, 450.f, 3.f, { 0.5f,0.5f,0.5f });
 	Engine::GetText(font2).Draw("Press O to main option", 0.f, 550.f, 3.f, { 0.5f,0.5f,0.5f });
 }
